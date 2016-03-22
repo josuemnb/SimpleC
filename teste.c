53bf788515b00f6f11d05a0ccb92ab83b8b35ba7
josuemnb <josuemnb@gmail.com>
2016-03-22 14:19:42 +0000

    Create teste.c

---
+++ teste.c
@@ -0,0 +1,22 @@
+#include "simple.h"
+#include "String.h"
+#include "Vector.h"
+#include "Map.h"
+
+void main() {
+//
+	Vector(string) *vi = new(Vector,string);
+	for(int i=0;i<10;i++)
+		vi->add(StringUtils.valueInt(i));
+	foreach(id,vi)
+		printf("%s\n",id);
+	vi->destroy();
+
+	Map(int,string) *m = new(Map,int,string);
+	for(int i=0;i<20;i++)
+		m->put(i,StringUtils.build("teste de getKey ", StringUtils.valueInt(i), " done"));
+
+
+	for(int i=0;i<10;i++)
+		m->setValue("ola",i);
+	puts(m->getValue(10));
